90fbccc9c97f3cbbf5cb2795c654bdd08c4c7a33
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-12-12 10:27:59 -0500

    Created for in loops

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -33,16 +33,19 @@ class ViewController: UIViewController {
         guard let correctAnswer = correctAnswerInput.text,
             correctAnswer.count > 0 else {correctStudentAnswerOutput.text = "Please enter a value."
                 return }
+        //Create variables to count the number of correct and incorrect answers
+        var numberCorrectAnswers = 0
+        var numberWrongAnswers = 0
+        
+        //Logic to check each character in the input
+        for (position, character) in studentAnswers.enumerated(){
+            
         
     }
-    
-    //Create variables to count the number of correct and incorrect answers
-    var numberCorrectAnswers = 0
-    var numberWrongAnswers = 0
-    
-    //Logic to check each character in the input
-    for (position, character)  in studentAnswers.enumerated() {
-        
+        for (position, character) in correctAnswer.enumerated(){
+            //index
+        }
+
     }
 
     
